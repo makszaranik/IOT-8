ddf78f7ba7c85df25caf6990187a7d322c4e4b8c
makszaranik <zaranikXZ@gmail.com>
2025-12-10 12:34:59 +0200

    host

--- function_app.py
+++ function_app.py
@@ -17,10 +17,11 @@ def eventhub_trigger(azeventhub: func.EventHubEvent):
     logging.info(f'Received message: {body} from {device_id}')
 
     soil_moisture = body['soil_moisture']
-    if soil_moisture > 42:
+    if soil_moisture > 238:
         direct_method = CloudToDeviceMethod(method_name='relay_on', payload='{}')
     else:
         direct_method = CloudToDeviceMethod(method_name='relay_off', payload='{}')
+
     logging.info(f'Sending direct method request for {direct_method.method_name} for device {device_id}')
 
     registry_manager_connection_string = os.environ['REGISTRY_MANAGER_CONNECTION_STRING']
